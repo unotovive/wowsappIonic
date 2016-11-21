0af9fd47eaf38ef1ecb9604fbc0909c18f9214e6
unotovive <takumini1139@gmail.com>
2016-11-22 03:48:20 +0900

    detailがんばってる

--- src/pages/ships/detail/detail.ts
+++ src/pages/ships/detail/detail.ts
@@ -12,7 +12,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
     num: number;
-    ship = { name: "Hermelin", teir: 1,type:"巡洋艦", hpA:100,hpB:200,hpC:300,lengA:10,lengB:10,lengC:10,speedA:30,speedB:40,hideA:11,hideB:12,hideC:14,tropA:"2*3",tropB:"3*3",tropC:"4*4",airgun:"FUCK"};
+    ship = { name: "Hermelin", teir: 1, type: "巡洋艦", hpA: 9000, hpB: null, hpC: null, lengA: 8.6, lengB: null, lengC: null, speedA: 24, speedB: null, hideA: 8.6, hideB: null, hideC: null, tropA: "", tropB: "", tropC: "", airgun: "Nothing" };
 
     constructor(public navCtrl: NavController, navParams: NavParams) {
         this.num = navParams.get("num");
@@ -21,10 +21,31 @@ export class DetailPage {
         console.log('Hello DetailPage Page');
         switch (this.num) {
             case 1:
-                this.ship ={ name: "Hermelin", teir: 1,type:"巡洋艦", hpA:100,hpB:200,hpC:300,lengA:10,lengB:10,lengC:10,speedA:30,speedB:40,hideA:11,hideB:12,hideC:14,tropA:"2*3",tropB:"3*3",tropC:"4*4",airgun:"FUCK"};
+                this.ship = { name: "Hermelin", teir: 1, type: "巡洋艦", hpA: 9000, hpB: null, hpC: null, lengA: 8.6, lengB: null, lengC: null, speedA: 24, speedB: null, hideA: 8.6, hideB: null, hideC: null, tropA: "", tropB: "", tropC: "", airgun: "Nothing" };
                 break;
             case 2:
-                this.ship = { name: "Hermelin", teir: 2,type:"巡洋艦", hpA:100,hpB:200,hpC:300,lengA:10,lengB:10,lengC:10,speedA:30,speedB:40,hideA:11,hideB:12,hideC:14,tropA:"2*3",tropB:"3*3",tropC:"4*4",airgun:"FUCK"};
+                this.ship = { name: "Dresden", teir: 2, type: "巡洋艦", hpA: 15800, hpB: 17100, hpC: null, lengA: 8.1, lengB: 8.9, lengC: null, speedA: 25.5, speedB: null, hideA: 9.5, hideB: null, hideC: null, tropA: "", tropB: "", tropC: "", airgun: "Nothing" };
+                break;
+            case 3:
+                this.ship={ name: "Emden", teir: 2,type:"巡洋艦", hpA:17100,hpB:null,hpC:null,lengA:8.9,lengB:null,lengC:null,speedA:24,speedB:null,hideA:9.5,hideB:null,hideC:null,tropA:"",tropB:"",tropC:"",airgun:"Nothing"};
+                break;
+            case 4:
+                this.ship ={ name: "Kolberg", teir: 3,type:"巡洋艦", hpA:17200,hpB:18300,hpC:null,lengA:8.3,lengB:9.1,lengC:null,speedA:27.6,speedB:null,hideA:9.7,hideB:null,hideC:null,tropA:"",tropB:"",tropC:"",airgun:"Nothing"};
+                break;
+            case 5:
+                this.ship ={ name: "karlsruhe", teir: 4,type:"巡洋艦", hpA:19700,hpB:22700,hpC:null,lengA:10.6,lengB:11.7,lengC:null,speedA:27.5,speedB:null,hideA:10.6,hideB:null,hideC:null,tropA:"2*2",tropB:"4*2",tropC:"",airgun:"Little"};
+                break;
+            case 6:
+                this.ship ={ name: "Königsberg", teir: 5,type:"巡洋艦", hpA:22600,hpB:24300,hpC:null,lengA:15.0,lengB:16.5,lengC:null,speedA:32.5,speedB:null,hideA:12.4,hideB:null,hideC:null,tropA:"4*3",tropB:"4*3",tropC:"",airgun:"Little"};
+                break;
+            case 7:
+                this.ship ={ name: "Nürnberg", teir: 6,type:"巡洋艦", hpA:23800,hpB:27000,hpC:null,lengA:15.0,lengB:16.5,lengC:null,speedA:32,speedB:null,hideA:12.6,hideB:null,hideC:null,tropA:"4*3",tropB:"4*3",tropC:"2*3",airgun:"middle"};
+                break;
+            case 8:
+                this.ship ={ name: "Yorck", teir: 7,type:"巡洋艦", hpA:27200,hpB:32600,hpC:null,lengA:15.7,lengB:17.3,lengC:null,speedA:32,speedB:null,hideA:13.9,hideB:null,hideC:null,tropA:"4*3",tropB:"",tropC:"",airgun:"little"};
+                break;
+            case 9:
+                this.ship ={ name: "A.Hipper", teir: 8,type:"巡洋艦", hpA:37300,hpB:43800,hpC:null,lengA:16.1,lengB:17.7,lengC:null,speedA:32,speedB:null,hideA:14.2,hideB:null,hideC:null,tropA:"4*3",tropB:"",tropC:"",airgun:"Strong"};
                 break;
         }
     }
